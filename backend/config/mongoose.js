f4dcab0a7f2963017e8f4e7b57085b472eaa0fd1
Muzzammil Hussain <095hussainmuzzammil@gmail.com>
2023-12-15 22:12:57 +0530

    db update

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const {DB_NAME} = require('../utils/constant')
 
 
 exports.connectDB = async ()=>{
     try {
 
-        const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
         console.log(`MONGO_DB connected : ${connectionInstance.connection.host}`)
 
         
